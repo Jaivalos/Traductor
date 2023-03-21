b462a0c285afa038b51f4785bf36a297b285e553
Javier Avalos <jaivalos18j2@gmail.com>
2023-03-20 22:05:45 -0600

    Prueba-de-commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	return 0;
+int main() {
+	
+	int edad = 19;
+	
+	return edad;
 }
