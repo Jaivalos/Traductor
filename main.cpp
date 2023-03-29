e27048afc58d110320a70320eafc5d09f674d9dc
Javier Avalos <jaivalos18j2@gmail.com>
2023-03-28 21:12:08 -0600

    Creacio-de-la-logica-para-seleccionar-idioma

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int x = 0;
-int retorno,f,y,temp;
+int retorno = 0,f,y,temp;
 string palabra;
 bool encontrado=false;
 
@@ -44,74 +44,130 @@ void CargarDatos() {
 	}
 }
 
+int menu(){
+	int op;
+	system("cls");
+	cout<<"1= Traducir palabra"<<endl;
+	cout<<"2= Reporte"<<endl;
+	cout<<"3= Salir"<<endl;
+	cin>>op;
+	return op;	
+}
+
+void reporte(){
+	for(x=0; x<=50; x++) {
+		// Imprimir
+		cout << "==================" << endl;
+		cout << "Espanol:  " << Pal[x].PalEs << endl;
+		cout << "Italiano: " << Pal[x].PalIt << endl;
+		cout << "Ingles:   " << Pal[x].PalIn << endl;
+		cout << "Frances:  " << Pal[x].PalFr << endl;
+		cout << "Aleman:   " << Pal[x].PalDe << endl;
+	}
+}
+
+int idioma_origen(){
+	int op;
+	cout<<"------\n Idioma de Origen:\n1. Espanol \n2. Italiano \n3. Ingles \n4. Frances \n5. Aleman \n(Escriba el numero asociado) \n------"<<endl;
+	cin >> op;
+	return op;
+}
+
+int idioma_destino(){
+	int op;
+	cout<<"------\n Idioma de Destino: \n1. Espanol \n2. Italiano \n3. Ingles \n4. Frances \n5. Aleman \n(Escriba el numero asociado) \n------"<<endl;
+	cin >> op;
+	return op;
+}
+
+string busqueda(int origen, int destino, string palabra){
+	if(origen == 1){
+		for(y=0; y<=50; y++) {
+			if(palabra==Pal[y].PalEs) {
+				encontrado=true;
+				break;
+			}
+		}
+	}else if(origen == 2){
+		for(y=0; y<=50; y++) {
+			if(palabra==Pal[y].PalIt) {
+				encontrado=true;
+				break;
+			}
+		}
+	}else if(origen == 3){
+		for(y=0; y<=50; y++) {
+			if(palabra==Pal[y].PalIn) {
+				encontrado=true;
+				break;
+			}
+		}
+	}else if(origen == 4){
+		for(y=0; y<=50; y++) {
+			if(palabra==Pal[y].PalFr) {
+				encontrado=true;
+				break;
+			}
+		}
+	}else if(origen == 5){
+		for(y=0; y<=50; y++) {
+			if(palabra==Pal[y].PalDe) {
+				encontrado=true;
+				break;
+			}
+		}
+	}
+	
+	if(encontrado == true){
+		if(destino == 1){
+			cout << "Espanol:  " << Pal[y].PalEs << endl;
+		}else if(destino == 2){
+			cout << "Italiano: " << Pal[y].PalIt << endl;
+		}else if(destino == 3){
+			cout << "Ingles:   " << Pal[y].PalIn << endl;
+		}else if(destino == 4){
+			cout << "Frances:  " << Pal[y].PalFr << endl;
+		}else if(destino == 5){
+			cout << "Aleman:   " << Pal[y].PalDe << endl;
+		}else{
+			cout<<"La palabra no existe!!!"<<endl;
+			encontrado=false;
+		}
+	}
+}
+
 int main() {
+	
 	CargarDatos();
 
-	do {
-		system("cls");
-		cout<<"1= Traducir palabra"<<endl;
-		cout<<"2= Reporte"<<endl;
-		cout<<"3= Salir"<<endl;
-		cin>>retorno;
-
+	do{
+		retorno = menu();
 		if(retorno==1) {
-			do {
+			do{
+				int origen  = idioma_origen();
+				int destino = idioma_destino();
+				
 				system("cls");
-				cout<<"INGRESE PALABRA"<<endl;
+				
+				cout<<"------ Ingrese una palabra ------"<<endl;
 				cin>>palabra;
-
-				for(y=0; y<=50; y++) {
-					if(palabra==Pal[y].PalEs) {
-						encontrado=true;
-						break;
-
-					}
-				}
-				if(encontrado==true) {
-					cout << "Espanol:  " << Pal[y].PalEs << endl;
-					cout << "Italiano: " << Pal[y].PalIt << endl;
-					cout << "Ingles:   " << Pal[y].PalIn << endl;
-					cout << "Frances:  " << Pal[y].PalFr << endl;
-					cout << "Aleman:   " << Pal[y].PalDe << endl;
-
-				} else {
-					cout<<"La palabra no existe!!!"<<endl;
-					encontrado=false;
-				}
-				cout<<endl<<endl<<"1= Traducir una nueva palabra 2= salir al menu principal"<<endl;
+				
+				string traduccion = busqueda(origen, destino, palabra);
+				
+				cout<<endl<<endl<<"1 = Traducir una nueva palabra \n2 = Volver al menu principal"<<endl;
 				cin>>f;
-
 			} while(f!=2);
 		}
-
 		if(retorno==2) {
-			system("cls");
 			do {
 				system("cls");
-				cout<<"REPORTE"<<endl;
-
-				for(x=0; x<=50; x++) {
-
-					// Imprimir
-					cout << "==================" << endl;
-					cout << "Espanol:  " << Pal[x].PalEs << endl;
-					cout << "Italiano: " << Pal[x].PalIt << endl;
-					cout << "Ingles:   " << Pal[x].PalIn << endl;
-					cout << "Frances:  " << Pal[x].PalFr << endl;
-					cout << "Aleman:   " << Pal[x].PalDe << endl;
-				}
-
-				cout<<endl<<endl<<"2= salir al menu principal"<<endl;
+				cout<<"------- Reporte -------"<<endl;
+				reporte();
+				cout<<"\n\n2: Salir al menú principal"<<endl;
 				cin>>f;
 			} while(f!=2);
-		}
-		if(retorno==3) {
-			system("exit");
 		}
 	} while(retorno!=3);
-
-
-
 }
 
 
