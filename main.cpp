c5aeba877d01c4ca25240cd28e77cecd9180696d
Javier Avalos <jaivalos18j2@gmail.com>
2023-06-03 07:11:27 -0600

    Proyecto-final

--- main.cpp
+++ main.cpp
@@ -24,7 +24,12 @@ struct Palabra {
 	string PalIn;
 	string PalFr;
 	string PalDe;
-} Pal[101];
+	string PalCodEs;
+	string PalCodIt;
+	string PalCodIn;
+	string PalCodFr;
+	string PalCodDe;
+} Pal[200];
 
 void CargarDatos() {
 	ifstream archivo(NOMBRE_ARCHIVO);
@@ -33,19 +38,29 @@ void CargarDatos() {
 	while (getline(archivo, linea)) {
 
 		stringstream stream(linea); // Convertir la cadena a un stream
-		string espanol, italiano, ingles, frances, aleman;
+		string espanol, italiano, ingles, frances, aleman, codEspanol, codItaliano, codIngles, codFrances, codAleman;
 		// Extraer todos los valores de esa fila
 		getline(stream, espanol, delimitador);
 		getline(stream, italiano, delimitador);
 		getline(stream, ingles, delimitador);
 		getline(stream, frances, delimitador);
 		getline(stream, aleman, delimitador);
+		getline(stream, codEspanol, delimitador);
+		getline(stream, codItaliano, delimitador);
+		getline(stream, codIngles, delimitador);
+		getline(stream, codFrances, delimitador);
+		getline(stream, codAleman, delimitador);
 
 		Pal[x].PalEs = espanol;
 		Pal[x].PalIt = italiano;
 		Pal[x].PalIn = ingles;
 		Pal[x].PalFr = frances;
 		Pal[x].PalDe = aleman;
+		Pal[x].PalCodEs = codEspanol;
+		Pal[x].PalCodIt = codItaliano;
+		Pal[x].PalCodIn = codIngles;
+		Pal[x].PalCodFr = codFrances;
+		Pal[x].PalCodDe = codAleman;
 
 		x = x + 1;
 
@@ -64,7 +79,8 @@ int menu(){
 	
 	cout<<"|      1. Traducir palabra"<<endl;
 	cout<<"|      2. Reporte"<<endl;
-	cout<<"|      3. Salir\n|\n|"<<endl;
+	cout<<"|      3. Agregar palabra"<<endl;
+	cout<<"|      4. Salir\n|\n|"<<endl;
 	
 	cout<<"|      Ingrese una opción: ";cin>>op;
 	
@@ -72,16 +88,47 @@ int menu(){
 	return op;	
 }
 
+void AgregarPalabra(){
+	ofstream  archivo("Palabras.csv", ios::app);
+	if(!archivo){
+		cout << "Error al añadir la palabra "<< endl;
+	} else {
+		string pe, pi, pf, pit, pa;
+		cout << "Ingresa la palabra en español:  "; 
+		cin >> pe;
+		cout << "Ingresa la palabra en ingles:   "; 
+		cin >> pi;
+		cout << "Ingresa la palabra en frances:  "; 
+		cin >> pf;
+		cout << "Ingresa la palabra en italiano: "; 
+		cin >> pit;
+		cout << "Ingresa la palabra en aleman:   "; 
+		cin >> pa;
+		
+		archivo << pe << ";" << pi << ";" << pf << ";" << pit << ";" << pa << endl;
+		archivo.close();
+		
+		cout << GREEN << "----------------Se ingresó la palabra con exito----------------"<< WHITE;
+		CargarDatos();
+		menu();
+	}
+}
+
 void reporte(){
-	for(x=0; x<=100; x++) {
+	for(x=0; x<=101; x++) {
 		// Imprimir
-		cout << "-----------------------" << endl;
-		cout << "		Espanol :   "  << Pal[x].PalEs << endl;
-		cout << "		Italiano:   "  << Pal[x].PalIt << endl;
-		cout << "		Ingles  :   "  << Pal[x].PalIn << endl;
-		cout << "		Frances :   "  << Pal[x].PalFr << endl;
-		cout << "		Aleman  :   "  << Pal[x].PalDe << endl;
-		cout << "-----------------------" << endl;
+		cout << "---------------------------------------------------------------------" << endl;
+		cout << "		Espanol :             "  << Pal[x].PalEs << endl;
+		cout << "		Espanol codificada:   "  << Pal[x].PalCodEs << endl;
+		cout << "		Italiano:             "  << Pal[x].PalIt << endl;
+		cout << "		Italiano codificada:  "  << Pal[x].PalCodIt << endl;
+		cout << "		Ingles  :             "  << Pal[x].PalIn << endl;
+		cout << "		Ingles codificada:    "  << Pal[x].PalCodIn << endl;
+		cout << "		Frances :             "  << Pal[x].PalFr << endl;
+		cout << "		Frances codificada:   "  << Pal[x].PalCodFr << endl;
+		cout << "		Aleman  :             "  << Pal[x].PalDe << endl;
+		cout << "		Aleman codificada:    "  << Pal[x].PalCodDe << endl;
+		cout << "---------------------------------------------------------------------" << endl;
 	}
 }
 
@@ -103,35 +150,35 @@ int idioma_destino(){
 
 void busqueda(int origen, int destino, string palabra){
 	if(origen == 1){
-		for(y=0; y<=100; y++) {
+		for(y=0; y<=200; y++) {
 			if(palabra==Pal[y].PalEs) {
 				encontrado=true;
 				break;
 			}
 		}
 	}else if(origen == 2){
-		for(y=0; y<=100; y++) {
+		for(y=0; y<=200; y++) {
 			if(palabra==Pal[y].PalIt) {
 				encontrado=true;
 				break;
 			}
 		}
 	}else if(origen == 3){
-		for(y=0; y<=100; y++) {
+		for(y=0; y<=200; y++) {
 			if(palabra==Pal[y].PalIn) {
 				encontrado=true;
 				break;
 			}
 		}
 	}else if(origen == 4){
-		for(y=0; y<=100; y++) {
+		for(y=0; y<=200; y++) {
 			if(palabra==Pal[y].PalFr) {
 				encontrado=true;
 				break;
 			}
 		}
 	}else if(origen == 5){
-		for(y=0; y<=100; y++) {
+		for(y=0; y<=200; y++) {
 			if(palabra==Pal[y].PalDe) {
 				encontrado=true;
 				break;
@@ -142,7 +189,8 @@ void busqueda(int origen, int destino, string palabra){
 	if(encontrado == true){
 		if(destino == 1){
 			
-			cout << LGREEN << "Espanol:  " << Pal[y].PalEs << endl;
+			cout << LGREEN << "Espanol           :  " << Pal[y].PalEs << endl;
+			cout << LGREEN << "Espanol codificado:  " << Pal[y].PalCodEs << endl;
 			string ruta = "C:\\dic\\es\\"; ruta = ruta.append(Pal[y].PalEs); ruta = ruta.append(".wav");
 			ruta_lpcstr = ruta.c_str();
 			cout <<WHITE<< ruta;
@@ -150,7 +198,8 @@ void busqueda(int origen, int destino, string palabra){
 		
 		}else if(destino == 2){
 		
-			cout <<LGREEN << "Italiano: " << Pal[y].PalIt << endl;
+			cout <<LGREEN << "Italiano           : " << Pal[y].PalIt << endl;
+			cout <<LGREEN << "Italiano codificado: " << Pal[y].PalCodIt << endl;
 			string ruta = "C:\\dic\\it\\"; ruta = ruta.append(Pal[y].PalIt); ruta = ruta.append(".wav");
 			ruta_lpcstr = ruta.c_str();
 			cout <<WHITE<< ruta;
@@ -158,7 +207,8 @@ void busqueda(int origen, int destino, string palabra){
 		
 		}else if(destino == 3){
 			
-			cout <<LGREEN << "Ingles:   " << Pal[y].PalIn << endl;
+			cout <<LGREEN << "Ingles           :   " << Pal[y].PalIn << endl;
+			cout <<LGREEN << "Ingles codificado:   " << Pal[y].PalCodIn << endl;
 			string ruta = "C:\\dic\\in\\"; ruta = ruta.append(Pal[y].PalIn); ruta = ruta.append(".wav");
 			ruta_lpcstr = ruta.c_str();
 			cout <<WHITE<< ruta;
@@ -166,7 +216,8 @@ void busqueda(int origen, int destino, string palabra){
 			
 		}else if(destino == 4){
 			
-			cout <<LGREEN << "Frances:  " << Pal[y].PalFr << endl;
+			cout <<LGREEN << "Frances           :  " << Pal[y].PalFr << endl;
+			cout <<LGREEN << "Frances codificado:  " << Pal[y].PalCodFr << endl;
 			string ruta = "C:\\dic\\fr\\"; ruta = ruta.append(Pal[y].PalFr); ruta = ruta.append(".wav");
 			ruta_lpcstr = ruta.c_str();
 			cout <<WHITE<< ruta;		
@@ -174,7 +225,8 @@ void busqueda(int origen, int destino, string palabra){
 		
 		}else if(destino == 5){
 			
-			cout <<LGREEN << "Aleman:   " << Pal[y].PalDe << endl;
+			cout <<LGREEN << "Aleman           :   " << Pal[y].PalDe << endl;
+			cout <<LGREEN << "Aleman codificado:   " << Pal[y].PalCodDe << endl;
 			string ruta = "C:\\dic\\de\\"; ruta = ruta.append(Pal[y].PalDe); ruta = ruta.append(".wav");
 			ruta_lpcstr = ruta.c_str();
 			cout <<WHITE<< ruta;
@@ -192,9 +244,7 @@ void busqueda(int origen, int destino, string palabra){
 	}
 
 int main() {
-	
-	
-	
+
 	CargarDatos();
 
 	do{
@@ -225,7 +275,12 @@ int main() {
 				cin>>f;
 			} while(f!=2);
 		}
-	} while(retorno!=3);
+		if(retorno==3) {
+			system("cls");	
+			cout<<"---------------------------------------------------\n\n		Ingrese todos los datos de la nueva palabra \n\n             -------------------------"<<ORANGE<<endl;
+			AgregarPalabra();
+		}
+	} while(retorno!=4);
 }
 
 
